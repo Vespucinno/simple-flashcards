de0422af47cc32c9d071f353a1b142decd3735c9
Yanuar Purnama <107106392+Vespucinno@users.noreply.github.com>
2026-08-15 19:39:47 +0700

    Added the Main Feature, such as: - Adding a new topic (that's contain card with the title, question, answer) - The amount of question is depends on how much will the users add in a topic - Users can't upload a img file for the cover of the topic or in the question (might work on it later) - Users still can't register nor sign into an account..

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options here */ output: "export", // Mengaktifkan static exports (menghasilkan folder 'out')
+  images: {
+    unoptimized: true, // GitHub Pages tidak mendukung fitur optimasi gambar bawaan Next.js
+  },
+  // HANYA JIKA url GitHub Pages Anda berbentuk: username.github.io/nama-repositori
+  basePath: "vespucinno.github.io/simple-flashcards/",
 };
 
 export default nextConfig;
