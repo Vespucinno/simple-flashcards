6f6fbf63c4cf1f61a6b45aaa8e92fefa9464e0dd
Yanuar Purnama <107106392+Vespucinno@users.noreply.github.com>
2026-08-22 16:38:29 +0700

    Deleting some unimportant file that's resulting a bug and error and exist due to some misconfiguration.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */ output: "export", // Mengaktifkan static exports (menghasilkan folder 'out')
-  images: {
-    unoptimized: true, // GitHub Pages tidak mendukung fitur optimasi gambar bawaan Next.js
-  },
-  // HANYA JIKA url GitHub Pages Anda berbentuk: username.github.io/nama-repositori
-  basePath: "/simple-flashcards",
+  reactStrictMode: true,
 };
-
 export default nextConfig;
